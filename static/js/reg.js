7c7eeee40bace99635ceacdafb7517fe6c54dc92
archar <464044128@qq.com>
2024-09-15 09:44:25 +0800

    2

---
+++ static/js/reg.js
@@ -0,0 +1,24 @@
+ function regms() {
+     window.open('http://35.220.152.149:8085?gg=msseo5');
+ }
+
+ function regvn() {
+     window.open('http://35.220.152.149:8081?gg=vnseo5');
+ }
+
+ function regyl() {
+     window.open('http://35.220.152.149:8082?gg=ylseo5');
+ }
+
+ function regsg() {
+     window.open('http://35.220.152.149:8083?gg=sgseo5');
+ }
+
+ function regub() {
+     window.open('http://35.220.152.149:8088?gg=dtgyi5');
+ }
+
+ function regdb() {
+     window.open('http://35.220.152.149:8086?gg=dbseo5');
+ }
+
